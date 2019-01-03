0314ec61e357dc38607ce585224e8d047546a847
velioo <velioocs@mail.bg>
2019-01-03 20:34:15 +0200

    Added system property for the textio.properties file

--- src/main/java/com/sap/App.java
+++ src/main/java/com/sap/App.java
@@ -2,7 +2,8 @@ package com.sap;
 
 public class App {
 	public static void main(String[] args) {
-
+		System.setProperty("textio.properties.location", "..\\textio.properties");
+		
 		Library library = new Library();
 		LibraryMenu libraryMenu = library.getLibraryMenu();
 
